2c868227c34f0c8c842d619cb7636f256ffe6e15
Julia Shaw <juliashaw@Julias-MacBook-Pro-3.local>
2024-05-17 12:43:33 -0400

    simplifying code

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import os
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, render_template
 from flask_session import Session
 
 from helpers import top_songs, allow_user, top_artists, read_playlists, currently_listening
@@ -25,58 +24,50 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    global sp
-    sp = None
-    return render_template("homepage.html", sp=sp)
-
-@app.route("/songs")
-def songs():
-    return render_template("songs.html")
+    global user
+    user = None
+    return render_template("homepage.html", user=user)
 
 @app.route("/shortsongs")
 def shortsongs():
-    songs_short, songs_medium, songs_long = top_songs(sp)
-    return render_template("shortsongs.html",songs=songs_short)
+    songs_short, songs_medium, songs_long = top_songs(user)
+    return render_template("songs.html", heading="top songs (last month)", songs=songs_short)
 
 @app.route("/mediumsongs")
 def mediumsongs():
-    songs_short, songs_medium, songs_long = top_songs(sp)
-    return render_template("mediumsongs.html",songs=songs_medium)
+    songs_short, songs_medium, songs_long = top_songs(user)
+    return render_template("songs.html", heading = "top songs (last six months)", songs=songs_medium)
 
 @app.route("/longsongs")
 def longsongs():
-    songs_short, songs_medium, songs_long = top_songs(sp)
-    return render_template("longsongs.html",songs=songs_long)
-
-@app.route("/artists")
-def artists():
-    return render_template("artists.html")
+    songs_short, songs_medium, songs_long = top_songs(user)
+    return render_template("songs.html", heading = "top songs (all time)", songs=songs_long)
 
 @app.route("/shortartists")
 def shortartists():
-    artists_short, artists_medium, artists_long = top_artists(sp)
-    return render_template("shortartists.html",artists=artists_short)
+    artists_short, artists_medium, artists_long = top_artists(user)
+    return render_template("artists.html", heading="top artists (last month)", artists=artists_short)
 
 @app.route("/mediumartists")
 def mediumartists():
-    artists_short, artists_medium, artists_long = top_artists(sp)
-    return render_template("mediumartists.html",artists=artists_medium)
+    artists_short, artists_medium, artists_long = top_artists(user)
+    return render_template("artists.html", heading="top artists (last six months)", artists=artists_medium)
 
 @app.route("/longartists")
 def longartists():
-    artists_short, artists_medium, artists_long = top_artists(sp)
-    return render_template("longartists.html",artists=artists_long)
+    artists_short, artists_medium, artists_long = top_artists(user)
+    return render_template("artists.html", heading="top artists (all time)", artists=artists_long)
 
 @app.route("/allow")
 def allow():
-    global sp
-    sp = allow_user()
-    return render_template('homepage.html', sp = sp)
+    global user
+    user = allow_user()
+    return render_template('homepage.html', user=user)
 
 @app.route("/logout")
 def logout():
-    sp = None
-    return render_template('homepage.html', sp=sp)
+    user = None
+    return render_template('homepage.html', user=user)
 
 @app.route("/homepage")
 def homepage():
@@ -84,10 +75,10 @@ def homepage():
 
 @app.route("/playlists")
 def playlists():
-    playlists = read_playlists(sp)
+    playlists = read_playlists(user)
     return render_template('playlists.html', playlists=playlists)
 
 @app.route("/listening")
 def listening():
-    listening = currently_listening(sp)
-    return render_template("listening.html", listening = listening)
+    listening = currently_listening(user)
+    return render_template("listening.html", listening=listening)
--- helpers.py
+++ helpers.py
@@ -1,34 +1,28 @@
-import os
-from cs50 import SQL
-
-from flask import redirect, render_template, request, session
-
-import sys
 import spotipy
 import spotipy.util as util
 
 from spotipy.oauth2 import SpotifyOAuth
 
-global sp
+global user
 
 client_id ='b02eba6bbf4a48a484c525c15555b616'
 client_secret = '3b9d813cd07e4bc5acfa39c3c3659dce'
 redirect_uri = "http://localhost:8080/callback"
 
-def top_songs(sp):
+def top_songs(user):
     scope = 'user-top-read'
 
     songs_short = []
     songs_medium= []
     songs_long=[]
 
-    short = sp.current_user_top_tracks(time_range='short_term', limit=50)
+    short = user.current_user_top_tracks(time_range='short_term', limit=50)
     for i, item in enumerate(short['items']):
         songs_short.append([str(i+1)+ '. ' +item['name']+ ' // '+ item['artists'][0]['name'], item['external_urls']['spotify']])
-    medium = sp.current_user_top_tracks(time_range='medium_term', limit=50)
+    medium = user.current_user_top_tracks(time_range='medium_term', limit=50)
     for i, item in enumerate(medium['items']):
         songs_medium.append([str(i+1) + '. ' +item['name']+ ' // '+ item['artists'][0]['name'], item['external_urls']['spotify']])
-    lng = sp.current_user_top_tracks(time_range='long_term', limit=50)
+    lng = user.current_user_top_tracks(time_range='long_term', limit=50)
     for i, item in enumerate(lng['items']):
         songs_long.append([str(i+1) + '. ' +item['name'] + ' // ' + item['artists'][0]['name'], item['external_urls']['spotify']])
 
@@ -36,45 +30,45 @@ def top_songs(sp):
 
 def allow_user():
     scope = 'user-top-read playlist-read-private user-read-currently-playing'
-
     token = util.prompt_for_user_token(scope, client_id = client_id, client_secret=client_secret, redirect_uri=redirect_uri)
     if token:
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope = scope, client_id = client_id,
+        user = spotipy.Spotify(auth_manager=SpotifyOAuth(scope = scope, client_id = client_id,
                             client_secret = client_secret, redirect_uri = redirect_uri))
     else:
-        sp = None
-    return sp
+        user = None
+    return user
 
-def top_artists(sp):
+# get users most listened to artists for all time, last six months, and
+def top_artists(user):
     scope = 'user-top-read'
 
     artists_short = []
-    artists_medium= []
-    artists_long=[]
+    artists_medium = []
+    artists_long = []
 
-    short = sp.current_user_top_artists(time_range='short_term', limit=10)
+    short = user.current_user_top_artists(time_range='short_term', limit=50)
     for i, item in enumerate(short['items']):
         artists_short.append([str(i+1)+ '. ' +item['name'], item['external_urls']['spotify']])
-    medium = sp.current_user_top_artists(time_range='medium_term', limit=10)
+    medium = user.current_user_top_artists(time_range='medium_term', limit=50)
     for i, item in enumerate(medium['items']):
         artists_medium.append([str(i+1) + '. ' +item['name'], item['external_urls']['spotify']])
-    lng = sp.current_user_top_artists(time_range='long_term', limit=10)
+    lng = user.current_user_top_artists(time_range='long_term', limit=50)
     for i, item in enumerate(lng['items']):
         artists_long.append([str(i+1) + '. ' +item['name'], item['external_urls']['spotify']])
 
     return (artists_short, artists_medium, artists_long)
 
-def read_playlists(sp):
+def read_playlists(user):
     scope = 'playlist-read-private'
     playlists = []
-    results = sp.current_user_playlists(limit=50)
+    results = user.current_user_playlists(limit=50)
     for i, item in enumerate(results['items']):
         playlists.append([str(i+1)+'.  '+ item['name'], item['external_urls']['spotify']])
     return playlists
 
-def currently_listening(sp):
+def currently_listening(user):
     scope = 'user-read-currently-playing'
-    results = sp.currently_playing()
+    results = user.currently_playing()
     if results:
        return [results['item']['name'] + ' // ' + results['item']['artists'][0]['name'], results['item']['external_urls']['spotify']]
     else:
